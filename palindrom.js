a6bead2a20bfca33a40ce93a9c8d418dcc845657
Babu Lal Mandal <madhukarbabulal5@gmail.com>
2023-10-06 07:37:34 +0530

    update

---
+++ palindrom.js
@@ -0,0 +1,16 @@
+let name = "aaabbaaa";
+
+let rev = "";
+for (let i=name.length-1;i>0;i--)
+{
+    rev += name[i];
+}
+
+if (name == rev)
+{
+    console.log("Yes");
+}
+else
+{
+    console.log("No");
+}
